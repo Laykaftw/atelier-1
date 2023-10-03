84180e702425b7cb48b57c5edf3a14808b0e2024
Laykaftw <azerbensassi@gmail.com>
2023-10-03 23:40:43 +0100

    Atelier1 Angular

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+})
+export class ProduitsComponent {
+
+  produits : string[];
+
+
+  constructor(){
+    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+  }
+
+  ngOnInit( ):void{
+
+  }
+}
